19aa52249830eb590615acb93f6b2199710545a0
Alberto Flores <alberto94fo@gmail.com>
2022-10-12 14:27:02 -0500

    create react app and add to-do-app components elements and styles

---
+++ src/TodoCounter.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./TodoCounter.css";
+
+/* const styles = {
+  color: 'red',
+  backgroundColor: 'yellow',
+}; */
+
+function TodoCounter() {
+  return (
+    <h2 className="TodoCounter">2 of 3 TODOs has been completed</h2>
+  );
+}
+
+export { TodoCounter };
